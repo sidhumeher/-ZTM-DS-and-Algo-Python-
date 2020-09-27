dc50d704be8190873f4515164ffc27670ae13f7b
Sid <sidhumeher@yahoo.co.in>
2020-09-27 11:11:53 -0700

    Adding Big O for stack operations

--- Stack/Stack.py
+++ Stack/Stack.py
@@ -25,6 +25,13 @@ class Node(object):
 
 class Stack():
     
+    '''
+    Lookup: O(n)
+    Pop: O(1)
+    Push: O(1)
+    Peek O(1)
+    '''
+    
     def __init__(self):
         self.head = None
         
