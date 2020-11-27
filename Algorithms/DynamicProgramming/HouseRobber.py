d4b47995c4eb3afad0f135b50fa1d7c2c0adb4f2
Sid <sidhumeher@yahoo.co.in>
2020-11-26 16:54:12 -0800

    Dynamic programming

--- Algorithms/DynamicProgramming/HouseRobber.py
+++ Algorithms/DynamicProgramming/HouseRobber.py
@@ -5,6 +5,7 @@ Created on Nov 25, 2020
 '''
 
 
+# Without dynamic programming
 def proRobber(nums):
     
     if nums == 0:
@@ -23,6 +24,35 @@ def proRobber(nums):
     maxMoney = max(p_num, q_num)
     return maxMoney
 
+# Dynamic programming
+'''
+If n = 1, maxamount = a1, n=2 maxamount = max(a1,a2)
+If n = 3, we have 2 options
+1. Rob third house and add 1st house amount
+2. Stick with maxamont of first and second house
+f(k) = max(a3+f(k-2),f(k-1))
+
+We can solve this with 2 variables keeping track of previous 2 max amounts
+'''
+
+
+def proRobber1(nums):
+    
+    if len(nums) == 0:
+        return 0
+    
+    prev_prev = 0; prev = 0; curr = 0
+    
+    for amount in nums:
+        prev = curr
+        curr = max(amount + prev_prev, prev)
+        prev_prev = prev
+        
+    return curr
+
+# Time complexity: O(n)
+# Space: O(1)
+
 
 if __name__ == '__main__':
     nums = [1, 2, 3, 1]
@@ -30,3 +60,10 @@ if __name__ == '__main__':
     
     nums = [2, 7, 9, 3, 1]
     print(proRobber(nums))
+    
+    # Using Dynamic programming
+    nums = [1, 2, 3, 1]
+    print(proRobber1(nums))
+    
+    nums = [2, 7, 9, 3, 1]
+    print(proRobber1(nums))
